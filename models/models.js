3d71ddbe1a5550a1b25bbff1781120834cab85a6
Antonio Prieto <antonio.prieto.riera@gmail.com>
2015-08-01 13:16:21 +0200

    Creada base de datos (no funciona)

---
+++ models/models.js
@@ -0,0 +1,28 @@
+var path = require('path');
+
+// Cargar Modelo OR
+var Sequelize = require('sequelize');
+
+// Usar BBDD SQLite;
+var sequelize = new Sequelize (null, null, null,
+						{dialect: "sqlite", storage: "quiz.sqlite"}
+					);
+
+// Importar la definición de la tabla Quiz en quiz.js
+var Quiz = sequelize.inport(path.join(__dirname, 'quiz'));
+
+// Exportar la definición de la tabla Quiz
+exports.Quiz = Quiz;
+
+// squelize.sync() crea e inicializa tabla de preguntas en DB
+squelize.sync().success(function() {
+	// success(...) ejecuta el manejador una vez creada la tabla
+	Quiz.count().success(funtion (count){
+		if(count === 0) { // La tabla se inicializa sólo si está vacía
+			Quiz.create({ pregunta: 'Capital de Italia',
+						  respuesta: 'Roma'
+						})
+			.success(function(){console.log('Base de datos inicializada')});
+		};
+	});
+});
