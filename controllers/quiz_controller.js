ab4eafe15a1b7517f179d9cbd22c1d39243e80d3
Antonio Prieto <antonio.prieto.riera@gmail.com>
2015-08-17 15:29:45 +0200

    Mod8: Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -59,6 +59,26 @@ exports.answer = function(req, res) {
 	})
 };
 
+// GET /quizes/new
+exports.new = function(req, res) {
+	var quiz = models.Quiz.build( // crea objeto quiz
+		{pregunta: "Pregunta", respuesta: "Respuesta"}
+	);
+
+	res.render('quizes/new', {quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build( req.body.quiz );
+
+	// guarda en BD los camos pregunta y respuesta de quiz
+	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+		res.redirect('/quizes');
+	})            // Redirección HTTP (URL relativo) a lista preguntas
+};
+
+
 // GET /author
 exports.author = function(req, res) {
 	res.render('author', {});	
