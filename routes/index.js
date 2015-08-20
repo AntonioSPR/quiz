3a469d2643d2737c367e475065a5e424c46e92ff
Antonio Prieto <antonio.prieto.riera@gmail.com>
2015-08-20 02:25:44 +0200

    Mod9: Login

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,6 +12,12 @@ router.get('/', function(req, res, next) {
 
 // Autoload de  comandos con :quizId
 router.param('quizId', quizController.load); //autoload :quizId
+
+// Rutas de sesión
+router.get('/login',  sessionController.new);      // Formulario login
+router.post('/login', sessionController.create);  // crear sesión
+router.get('/logout', sessionController.destroy); // destruir sesión
+
 
 // Definición de las rutas de /quizes 
 router.get('/quizes',                      quizController.index);
@@ -23,6 +30,7 @@ router.put('/quizes/:quizId(\\d+)',        quizController.update);
 router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
 router.get('/author',                      quizController.author);
 
+// Rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
 
